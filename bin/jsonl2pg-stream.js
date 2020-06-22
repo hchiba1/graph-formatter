e3fcbd5578294d10a1e4955a236f3e42ca450782
Hirokazu Chiba <hchiba1@gmail.com>
2020-06-22 17:37:52 +0900

    update jsonl2pg-stream.js

--- bin/jsonl2pg-stream.js
+++ bin/jsonl2pg-stream.js
@@ -12,8 +12,44 @@ if(commander.args[0]) {
 } else {
   const reader = readline.createInterface(process.stdin);
   reader.on('line', (line) => {
-    outputJsonLines(line);
+    outputPG(line);
   });
   process.stdin.on('end', () => {
   });
+}
+
+// Functions
+function outputPG(line) {
+  obj = JSON.parse(line);
+  array = []
+  if (obj.type == "node") {
+    array.push(obj.id);
+    obj.labels.forEach(lab => {
+      array.push(`:"${lab}"`);
+    });
+    const props = Object.keys(obj.properties);
+    props.forEach(p => {
+      obj.properties[p].forEach(val => {
+        array.push(`"${p}":"${val}"`);
+      });
+    });
+  } else if (obj.type == "edge") {
+    if (obj.undirected) {
+      array.push(`${obj.from} -- ${obj.to}`);
+    } else {
+      array.push(`${obj.from} -> ${obj.to}`);
+    }
+    obj.labels.forEach(lab => {
+      array.push(`:"${lab}"`);
+    });
+    const props = Object.keys(obj.properties);
+    props.forEach(p => {
+      obj.properties[p].forEach(val => {
+        array.push(`"${p}":"${val}"`);
+      });
+    });
+  } else {
+    array.push(`Invalid line: ${line}`);
+  }
+  console.log(array.join('\t'))
 }

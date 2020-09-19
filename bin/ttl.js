c218d60dd71fd5ac5103c4d1055a3b030d48cd70
Hirokazu Chiba <hchiba1@gmail.com>
2020-09-20 05:34:41 +0900

    update bin/ttl.js

--- bin/ttl.js
+++ bin/ttl.js
@@ -7,6 +7,7 @@ const commander = require('commander')
       .arguments('<TTL_FILE>')
       .option('-c, --count', 'count triples')
       .option('--check', 'check')
+      .option('--nt', 'N-triples')
       .option('-d, --debug', 'output parsed synatax tree')
       .parse(process.argv);
 
@@ -34,6 +35,22 @@ try {
 
 if (commander.debug) {
   console.log(JSON.stringify(objectTree, null, 2));
+  process.exit(0);
+} else if (commander.nt) {
+  objectTree.triples.forEach((line) => {
+    if (line.hasOwnProperty('prefix')) {
+    } else {
+      // console.log(`<${line.s.iri}> <${line.p.iri}> `);
+      process.stdout.write(`<${line.s.iri}> <${line.p.iri}> `);
+      if (line.o.iri) {
+        console.log(`<${line.o.iri}> .`);
+      } else if (line.o.literal) {
+        console.log(`"${line.o.literal}" .`);
+      } else {
+        console.log();
+      }
+    }
+  });
   process.exit(0);
 } else if (commander.count) {
   console.log(objectTree.tripleCount);
